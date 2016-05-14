d7c5d98bbf9b2f1be8a52bd252123c5cb07f2cc5
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-05-15 01:53:30 +0300

    Backend: create update

--- back/src/main/java/com/kpicoop/controllers/rest/UserController.java
+++ back/src/main/java/com/kpicoop/controllers/rest/UserController.java
@@ -3,10 +3,7 @@ package com.kpicoop.controllers.rest;
 import com.kpicoop.model.User;
 import com.kpicoop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/rest/api/v1/users/")
@@ -21,9 +18,9 @@ public class UserController {
         return user;
     }
 
-    @RequestMapping(value = "{userId}", method = RequestMethod.GET)
-    public User updateUser(@PathVariable int userId) {
-        User user = userService.findById(userId);
-        return user;
+    @RequestMapping(value = "{userId}", method = RequestMethod.PUT)
+    public User updateUser(@PathVariable int userId, @RequestBody User user) {
+        User newUser = userService.saveUser(user);
+        return newUser;
     }
 }
