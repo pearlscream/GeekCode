91e7b8426fd3874338bf105b11010ff834e2e306
Evhenii Hudzenko <ukraineboyzheka@gmail.com>
2016-05-14 15:13:00 +0300

    main page

---
+++ front/app/scripts/controller/mainController.js
@@ -0,0 +1,36 @@
+angular.module('koya').controller('mainController', ['$scope',
+function($scope) {
+	var myTab = function(tabHeader, tabTitleClass, tabItemClass){
+		this.tabItems = $('.'+tabItemClass).toArray();
+		this.tabTitles = $('.'+tabTitleClass).toArray();
+
+		this.selectedItem = tabItemClass +'_selected';
+		this.selectedTitle = tabTitleClass +'_selected';
+
+
+		var __self = this;
+
+		tabHeader.click(function(e){
+		e.preventDefault();
+
+    var tabIndex = $(e.target).data('index');
+
+    if($(__self.tabItems[tabIndex]).hasClass(__self.selectedItem)) return;
+
+    $.each(__self.tabTitles, function(index, item){
+				$(item).removeClass(__self.selectedTitle);
+        });
+
+    $(__self.tabTitles[tabIndex]).addClass(__self.selectedTitle);
+
+		$.each(__self.tabItems, function(index, item){
+				$(item).removeClass(__self.selectedItem);
+        });
+
+		$(__self.tabItems[tabIndex]).addClass(__self.selectedItem);
+	});
+
+	};
+
+	var firstTabs = new myTab($('.validation-block__header'),'validation-block__item','validation-block__content');
+}]);
