b29779c810fb535f3ccf076bd9f865a1baaef311
cawo4ok <cawo4ok@gmail.com>
2016-05-14 21:35:37 +0300

    id page

--- front/app/scripts/controller/projectController.js
+++ front/app/scripts/controller/projectController.js
@@ -1,11 +1,19 @@
-angular.module('koya').controller('projectController', ['$scope',
-  function($scope) {
+angular.module('koya').controller('projectController', ['$scope','$state', '$stateParams','$http',
+  function($scope, $state, $stateParams, $http) {
     $scope.aceOptions = {
       theme: 'textmate',
       mode: 'r',
       useWrapMode: true,
       workerPath: '/styles/'
     }
+    $scope.id = $state.params.id;
+    console.log($scope.id);
+    if($scope.id!="undefined")
+    $http.get("http://10.55.33.56:8082/rest/api/v1/project/"+$scope.id)
+    .success(function(data) {
+                $scope.projects = data;
+                console.log($scope.projects);
+    });
     $scope.example = 'require(rCharts)\nrPlot(mpg ~ wt, data = mtcars,type = "point")';
     $scope.alertData = function(){
       console.log($scope.example);
